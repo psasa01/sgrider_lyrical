fb2581953a41510901d304658d2765c4ea5cb3e1
unknown <psasa01@gmail.com>
2018-06-18 15:56:55 +0200

    180618

--- client/index.js
+++ client/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+import SongList from './components/SongList';
+
+const client = new ApolloClient({});
 
 const Root = () => {
-  return <div>
-
-
-
-  </div>
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <SongList/>
+      </div>
+    </ApolloProvider>)
 };
 
 ReactDOM.render(
