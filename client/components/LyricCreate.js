7b762c518f81fbf2f8eb109ee5d298a4d1ccddfd
unknown <psasa01@gmail.com>
2018-06-21 15:50:33 +0200

    210618

---
+++ client/components/LyricCreate.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import addLyricToSong from '../queries/addLyricToSong';
+import fetchSingleSong from '../queries/fetchSingleSong';
+import { graphql } from 'react-apollo';
+
+class LyricCreate extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { content: '' };
+    }
+    onSubmit() {
+        event.preventDefault();
+
+        this.props.mutate({
+            variables: {
+                content: this.state.content,
+                songId: this.props.songId
+            }
+        }).then(() => this.setState({ content: '' }))
+
+    }
+    render() {
+        return (
+            <form onSubmit={this.onSubmit.bind(this)} >
+                <label>Add a Lyric</label>
+                <input 
+                    value={this.state.content}
+                    onChange={event => this.setState({ content: event.target.value })}
+                />
+            </form>
+        )
+    }
+}
+
+export default graphql(addLyricToSong)(LyricCreate);
--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import fetchSingleSong from '../queries/fetchSingleSong';
 import { Link } from 'react-router';
+import LyricCreate from './LyricCreate';
+import LyricList from './LyricList';
 
 class SongDetail extends Component {
 
@@ -17,6 +19,8 @@ class SongDetail extends Component {
                 <div>
                     <Link to="/">Back</Link>
                     <h3>{song.title}</h3>
+                    <LyricList lyrics={song.lyrics} />
+                    <LyricCreate songId={this.props.params.id} />
                 </div>
             )
         }
