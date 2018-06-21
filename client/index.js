2cc3bd41c7cb3388138f05d8f55678a104593ff3
unknown <psasa01@gmail.com>
2018-06-21 15:50:50 +0200

    210618

--- client/index.js
+++ client/index.js
@@ -8,7 +8,9 @@ import SongCreate from './components/SongCreate';
 import SongDetail from './components/SongDetail';
 import SongList from './components/SongList';
 
-const client = new ApolloClient({});
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id
+});
 
 const Root = () => {
   return (
