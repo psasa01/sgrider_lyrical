5c6f86ee5b95cf4feffff18b6d4d7fad465d3dd4
eminasale <psasa01@gmail.com>
2018-06-23 00:22:44 +0200

    230618

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -5,28 +5,31 @@ import { graphql } from 'react-apollo';
 class LyricList extends Component {
     onLike(id) {
         this.props.mutate({ variables: { id } })
-    
+
     }
     renderLyrics() {
-        return this.props.lyrics.map(({ id, content }) => {
-            return(
+        return this.props.lyrics.map(({ id, content, likes }) => {
+            return (
                 <li className="collection-item" key={id}>
+
                     {content}
-                    <i 
+
+                    <i
                         className="material-icons right"
-                        style={{cursor: 'pointer'}}
+                        style={{ cursor: 'pointer' }}
                         onClick={() => {
                             this.onLike(id)
                         }}
                     >
                         thumb_up
                     </i>
+                    <span className="badge right tile new" data-badge-caption="">{likes}</span>
                 </li>
             )
         })
     }
     render() {
-        
+
         return (
             <ul className="collection">
                 {this.renderLyrics()}
